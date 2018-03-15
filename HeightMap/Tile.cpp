ed64ff147fbd0876f090c0da8c111ffc98f9bc50
4tyTwo <pwutag33@yandex.ru>
2018-03-15 17:42:53 +0300

    Added Tile class, this branch is going to be master if succeed

--- HeightMap/Tile.cpp
+++ HeightMap/Tile.cpp
@@ -1,11 +1,38 @@
 #include "Tile.h"
 
-
-
-Tile::Tile()
-{
+Tile::Tile(){ // онструктор по-умолчанию, особого смысла не несет
+  temperature_ =-273;
+  humidity_ = 0.0;
+  height_ = 0.0;
+  type_ = "none";
 }
 
+void Tile::setHeight(double height) {
+  if (height < 0.0)
+    height = 0.0;
+  else
+    if (height > 1.0)
+      height = 1.0;
+  height_=height;
+}
+
+void Tile::setHumid(float humid) {
+  if (humid < 0.0)
+    humid = 0.0;
+  else
+    if (humid > 1.0)
+      humid = 1.0;
+  humidity_ = humid;
+}
+
+void Tile::setTemp(int temp) {
+  temperature_ = (temp<-273) ? -273 : temp;
+}
+
+void Tile::setType(std::string type) {
+  if (std::find(type_vect.begin(), type_vect.end(), type) != type_vect.end())
+    type_ = type;
+}
 
 Tile::~Tile()
 {
--- HeightMap/Tile.h
+++ HeightMap/Tile.h
@@ -1,15 +1,41 @@
 #pragma once
-enum special {water,mountain,none};
+#include <string>
+#include <vector>
+#include <algorithm>
+
+static const std::vector<std::string> type_vect{"water","mountain","sand","none"};//Еще биомы
 class Tile
 {
 public:
-  Tile();
-  ~Tile();
-private:
-special type;//Сделать enum
-double height_;//Альтитуда
-int temperature_;//Температура
-double humidity_;//Влажность
+  //Конструкторы
 
+  Tile();
+
+  //Set методы
+
+  void setHeight(double height);
+  void setTemp(int temp);
+  void setHumid(float humid);
+  void setType(std::string type);
+
+  //Get методы
+
+  inline double Height() { return height_; }
+  inline int Temperature() {return temperature_; }
+  inline float Humidity() { return humidity_; } 
+  inline std::string Type() { return type_; }
+
+  //Деструктор по умолчанию
+
+  ~Tile();
+  //inline std::string Type() { return type_; }
+
+  enum class special { water, mountain, sand, none }; //Биомы тоже наверное пойдут сюда
+
+private:
+  std::string type_;//Тип
+  double height_;//Альтитуда от 0 до 1.
+  int temperature_;//Температура в градусах цельсия, ограничена только снизу по очевидным причинам
+  float humidity_;//Влажность. Выражается в нормальизованном виде от 0 до 1, где 0 - полная сухость.
 };
 
